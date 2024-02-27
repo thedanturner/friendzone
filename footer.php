7dc29bcd42449c2d01b27bd27a1bc0fd16186e28
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-02-27 05:17:52 +0000

    Initial Build

---
+++ footer.php
@@ -0,0 +1,13 @@
+  <!-- footer -->
+  <footer class="container text-center">
+    <ul class="nav nav-pills pull-right">
+      <li>FriendZone - Made by Daniel Turner</li>
+    </ul>
+  </footer>
+  <!-- ./footer -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
+
+<?php $conn->close(); ?>
