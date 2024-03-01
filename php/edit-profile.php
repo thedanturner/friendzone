c8d983e9551be33e82b780c2d4795777beee89d8
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-03-01 07:42:36 +0000

    update additional profile details

--- php/edit-profile.php
+++ php/edit-profile.php
@@ -3,9 +3,9 @@
 
   db_connect();
 
-  $sql = "UPDATE users SET status = ?, location = ? WHERE id = ?";
+  $sql = "UPDATE users SET status = ?, location = ?, mobile = ?, email = ? WHERE id = ?";
   $statement = $conn->prepare($sql);
-  $statement->bind_param('ssi', $_POST['status'], $_POST['location'], $_SESSION['user_id']);
+  $statement->bind_param('ssssi', $_POST['status'], $_POST['location'], $_POST['mobile'], $_POST['email'], $_SESSION['user_id']);
 
   if ($statement->execute()) {
     redirect_to("../profile.php?username={$_SESSION['user_username']}");
